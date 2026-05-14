9e0201518791b724f67853380b6b49d5b874c93e
jnjkhjlkjhb8 <y0910420427@gmail.com>
2026-05-15 00:42:56 +0800

    盲人摸象

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.12.0
 	github.com/go-redis/redis v6.15.9+incompatible
 	google.golang.org/protobuf v1.36.11
+	resty.dev/v3 v3.0.0-beta.6
 )
 
 require (
@@ -18,6 +19,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.30.1 // indirect
+	github.com/go-resty/resty/v2 v2.17.2 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/goccy/go-yaml v1.19.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
--- server/functions/main.go
+++ server/functions/main.go
@@ -2,24 +2,27 @@ package main
 
 import (
 	"fmt"
-
-	_ "net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
+	"github.com/go-resty/resty/v2"
 )
 
-var r *gin.Engine
-
 func main() {
+	r := gin.Default()
+	c := resty.New()
 	rc := connectredis()
-	token := getToken(rc)
+	c.SetBaseURL("https://tdx.transportdata.tw/api/basic")
+	c.SetHeader("Content-Type", "application/json")
+	c.SetAuthToken(getToken(rc, c))
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 	r.Run(":8080")
+	defer rc.Close()
 }
 func connectredis() *redis.Client {
 	client := redis.NewClient(&redis.Options{
@@ -28,15 +31,31 @@ func connectredis() *redis.Client {
 		DB:       0,
 	})
 	pong, err := client.Ping().Result()
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println(pong, err)
 	return client
 }
-func getToken(rc *redis.Client) string {
+func getToken(rc *redis.Client, c *resty.Client) string {
 	val, err := rc.Get("TDX_Token").Result()
-	if err == nil {
-		r.GET("/", func(c *gin.Context) {
-			c.JSON(200, gin.H{})
-		})
+	if err != nil {
+		fmt.Println("Token eq Empty")
+		resp, err := c.R().
+			SetHeader("content-type", "application/x-www-form-urlencoded").
+			SetFormData(map[string]string{
+				"grant_type":    "client_credentials",
+				"client_id":     os.Getenv("TDX_CLIENT_ID"),
+				"client_secret": os.Getenv("TDX_CLIENT_SECRET"),
+			}).
+			Post("https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		err = rc.Set("TDX_Token", resp.String(), 86390).Err()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
 	}
 	return val
 }
